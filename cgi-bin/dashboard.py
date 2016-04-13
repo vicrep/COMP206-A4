740ae4027c0e177bffdc718cad7f707243a77b01
Joanna PINEDA <jpined1@linux.CS.McGill.CA>
2016-04-13 15:01:12 -0400

    Dashboard is dashing

--- cgi-bin/dashboard.py
+++ cgi-bin/dashboard.py
@@ -8,22 +8,34 @@ print
 print "<html>"
 print "<head><link rel=\"stylesheet\" type=\"text/css\" href=\"..\css\main.css\"></head>"
 print "<body>"
-print "<div class=\"dashboard\">"
-print "<h2>Welcome to your Dashboard</h2>"
+
+usr = form.getvalue('username', 'No user')
 
 #menu logout, make a friend, and see a friend
 print "<div class=\"mymenu\">"
 print "<ul>"
-print " <li><a href=\"../\"> Logout </a>"
-print " <li><a href=\"#\"> Make A Friend </a>"
-print " <li><a href=\"#\"> See A Friend </a>"
+print " <li><form action=\"./\" method =\"post\" target=\"_blank\">"
+print "  <input type=\"hidden\" name=\"username\" value=\"_\"></br>"
+print "  <button type=\"submit\" class=\"bg-success\">LOGOUT</button>"
+print "</form>"
+print " <li><form action=\"./makefriends.py\" method =\"post\" target=\"_blank\">"
+print "  <input type=\"hidden\" name=\"username\" value=", usr,"></br>"
+print "  <button type=\"submit\" class=\"bg-success\">MAKE A FRIEND</button>"
+print "</form>"
+print " <li><form action=\"./seeafriend.py\" method =\"post\" target=\"_blank\">"
+print "  <input type=\"hidden\" name=\"username\" value=\"1\"></br>"
+print "  <button type=\"submit\" class=\"bg-success\">SEE A FRIEND</button>"
+print "</form>"
 print "</ul>"
 print "</div>"
 
+print "<div class=\"dashboard\">"
+print "<h2>Welcome to your Dashboard</h2>"
 #status
 print "<div class=\"statusUpdate\">"
-print "<form name=\"input\" action=\"http://cgi.cs.mcgill.ca/~jpined1/cgi-bin/status.py\" method=\"get\">"
+print "<form name=\"input\" action=\"./status.py\" method=\"get\">"
 print "  <b>Status: <input type=\"text\" name=\"status\"></br>"
+print "  <input type=\"hidden\" name=\"username\" value=", usr, "></br>"
 print "  <button type=\"submit\" class=\"bg-success\">Update</button>"
 print "</form>"
 print "</div>"
@@ -31,11 +43,13 @@ print "</div>"
 #20 most recent status updates
 print "<h1>Newsfeed</h1>"
 print "<div class=\"newsfeed\">"
-
+friends=''
+line=''
 with open("friends.txt") as friend:
         for line in friend:
-                if line.split(None,1)[0] == 'bob11':
+                if line.split(None,1)[0] == usr:
                         friends = line
+friends = line + usr
 friendsList = re.sub("[^\w] ", " ", friends).split()
 statusFile = open("status.txt",'r')
 lines = statusFile.readlines()
@@ -44,15 +58,15 @@ lines.reverse()
 count = 0
 for line in lines:
         if count < 20:
-                #if line.split(None,1)[0] in friendsList:
-                statusUpdate = line
-                friendName = line.split(' ',1)[0]
-                statusUpdate = line.split(' ',1)[1]
-                print "<div class=\"post\">"
-                print "<p><b>"+ friendName + "</b></br></p>"
-                print "<p>" + statusUpdate
-                print "</div>"
-                count += 1
+                if line.split(None,1)[0] in friendsList:
+                	statusUpdate = line
+                	friendName = line.split(" ",1)[0]
+                	statusUpdate = line.split(" ",1)[1]
+                	print "<div class=\"post\">"
+                	print "<p><b>"+ friendName + "</b></br></p>"
+                	print "<p>" + statusUpdate
+                	print "</div>"
+                	count += 1
 
 print "</div>"
 print "</div>"
--- cgi-bin/status.py
+++ cgi-bin/status.py
@@ -2,7 +2,7 @@
 import cgi, cgitb
 
 form = cgi.FieldStorage()
-
+usr = form.getvalue('username', 'No User')
 print("Content-Type: text/html")    # HTML is following
 print
 print "<html>"
@@ -10,9 +10,13 @@ print "<head><title>Status Update</title><link rel=\"stylesheet\" type=\"text/cs
 
 if form.getvalue('status'):
         with open("./status.txt","a") as myfile:
-                line = "username " + form.getvalue('status')
+                line = form.getvalue('username') + ";" +form.getvalue('status')
                 myfile.write(line+"\n")
         print "<p>Status Updated"
 else:
         print "<p>Status Empty"
+print "<form action=\"./dashboard.py\" method =\"post\" target=\"_blank\">"
+print "  <input type=\"hidden\" name=\"username\" value=", usr,"></br>"
+print "  <button type=\"submit\" class=\"bg-success\">Continue to Dashboard</button>"
+print "</form>"
 print "</html>"
