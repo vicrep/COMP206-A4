722ca3753edae1c7557d78c4e89b4835bcae0c81
Victor Repkow <repkowvictor@gmail.com>
2016-04-15 12:48:26 -0400

    fixed indentation issues

--- cgi-bin/status.py
+++ cgi-bin/status.py
@@ -11,20 +11,19 @@ print "<html>"
 print "<head><title>Status Update</title><link rel=\"stylesheet\" type=\"text/css\" href=\"../css/main.css\"></head>"
 
 if form.getvalue('status'):
+	line = form.getvalue('username') + ";" +form.getvalue('status')
 	if os.stat("./status.txt").st_size == 0:
 		with open("./status.txt","a") as f:
-			line=form.getvalue('username') + ";" +form.getvalue('status')
 			f.write(line)
 	else:
-        	#with open("./status.txt","a") as f:
-                line = form.getvalue('username') + ";" +form.getvalue('status')
-        	with open("./status.txt", 'r+') as f:
-        		content = f.read()
-        		f.seek(0, 0)
-        		f.write(line.rstrip('\r\n') + '\n' + content)
+		# with open("./status.txt","a") as f:
+		with open("./status.txt", 'r+') as f:
+			content = f.read()
+			f.seek(0, 0)
+			f.write(line.rstrip('\r\n') + '\n' + content)
 	print "<p>Status Updated"
 else:
-        print "<p>Status Empty"
+	print "<p>Status Empty"
 print "<form action=\"dashboard.py\" method =\"post\" target=\"_self\"><input type=\"hidden\" name=\"username\" value=\"" + CURRENT + "\"><button type=\"submit\"> GO BACK TO MY DASHBOARD </button></form>"
 print "</form>"
 print "</html>"
